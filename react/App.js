f157232b12604be52e1ecd53f894cc21ec99f05e
firayenate <firaoldesalegn680@gmail.com>
2026-02-20 13:53:09 -0800

    react.js

---
+++ react/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Hello from './components/Functional_comp';
+function App() {
+  return (
+    <div className="App">
+   <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/components/Functional_comp.js
@@ -0,0 +1,5 @@
+import react from 'react';
+function Hello(){
+    return <h1>Functional Components</h1>
+}
+export default Hello;
